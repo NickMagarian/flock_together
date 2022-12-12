9d4ac3170d1ca62301c07b61e0f1740af82fcf2f
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-12-12 13:51:34 -0500

    user model drafted

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,41 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema ({
-    name: {
+    username: {
         type: String,
-        required: true,
+        trim: true,
+        required: "Username is required!",
         unique: true,
     },
-});
+
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is required!",
+        validate: [ 
+            function (input) {
+                return input.length >= 6;
+
+            },
+            "Password should be atleast 6 characters long!"
+        ]
+    },
+
+    userCreated: {
+        type: Date,
+        default: Date.now
+    },
+    email: {
+        type: String,
+        index: {
+            unique: true,
+            partialFilterExpression: {email:"string"}}
+        },
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    }
+);
+    
+
 
 const User = model('User', UserSchema);
 
