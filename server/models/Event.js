b2f828e9aaf46a955dc64c6aa78d8c2267dae21e
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-12-12 13:59:12 -0500

    event model created

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,5 +1,37 @@
 const { Schema, model } = require('mongoose');
 
  const EventSchema  = new Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+        validate: [
+            function(input) {
+                return input.length >= 1;
+            },
+            "Title Should Be Longer!"
+        ]
+    },
+    start: {
+        type: Date,
+        required: true
+    },
+    end: Date,
+    description: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    lastUpdated: Date
+    });
+
+//last updated date
+
+    EventSchema.methods.lastUpdatedDate = function() {
+        this.lastUpdated = Date.now();
+        return this.lastUpdated;
+    };
+    const Event = model("Event", EventSchema);
+
+    module.exports = Event;
     
-})
