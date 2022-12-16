66112ec960d4a07315f83292acdf41801fcb9075
Alexandria Rogers <rogers@Alexandrias-MBP.attlocal.net>
2022-12-15 21:32:07 -0500

    working group chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,46 @@
-import CalendarPage from "./pages/calendar.js"
+// import CalendarPage from "./pages/calendar.js"
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <div className="App">
+//        <Router>
+//       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+//         <Routes>
+//           <Route 
+//             path="/" 
+//             element={<CalendarPage/>} 
+//           />
+         
+//         </Routes>
+//       </div>
+//     </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EventChat from "./pages/events"
 
 function App() {
-  return (
-    <div className="App">
-       <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Routes>
-          <Route 
-            path="/" 
-            element={<CalendarPage/>} 
-          />
-         
-        </Routes>
+    return (
+      <div className="App">
+         <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<EventChat/>} 
+            />
+           
+          </Routes>
+        </div>
+      </Router>
       </div>
-    </Router>
-    </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
