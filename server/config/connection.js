8a955965a8b18fcbf6348261c7911de81d5e0790
NickMagarian <nickmagarian@gmail.com>
2022-12-19 15:54:08 -0500

    changed db connection link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/flock',
+  process.env.MONGODB_URI || 'mysql://zo51v23qd4f3v5oj:lrfsr92mick63vv1@ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g5rkg50j1qwlylhm',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
